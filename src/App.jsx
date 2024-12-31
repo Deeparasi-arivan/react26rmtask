71db4230a68c3bd1a389a2448ae8849989299e3e
Deeparasi-arivan <deepa.deeparasi17@gmail.com>
2024-12-31 22:08:12 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import FullStack from "./components/FullStack";
+import DataScience from "./components/DataScience";
+import CyberSecurity from "./components/CyberSecurity";
+import Career from "./components/Career";
+
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fullstack" element={<FullStack />} />
+          <Route path="/datascience" element={<DataScience />} />
+          <Route path="/cybersecurity" element={<CyberSecurity />} />
+          <Route path="/career" element={<Career />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/DataScience.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar.jsx"; // Explicit file extension
+
+
+const Home = () => {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="display-4">All Courses</h1>
+      <p className="lead">Welcome to the courses page! Select a category from the navigation menu.</p>
+    </div>
+  );
+};
+
+export default Home;
